c9529fb3c43b3fe5dc6ee5f478a9b7428f1b801e
petrero <petrero@gmail.com>
2022-11-10 22:50:30 +0200

    2.17 Get rid off the border in select

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="filters flex space-x-6">
         <div class="w-1/3">
-            <select name="category" id="category" class="w-full rounded-xl px-4 py-2">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Category One">Category One</option>
                 <option value="Category Two">Category Two</option>
                 <option value="Category Three">Category Three</option>
