a79198ae1890e244d029ab4ce2c0a562475a24cf
petrero <petrero@gmail.com>
2022-11-10 22:10:24 +0200

    2.7 Create logout link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,15 @@
                 @if (Route::has('login'))
                     <div class="px-6 py-4">
                         @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <a href="{{route('logout') }}"
+                                   onclick="event.preventDefault(); this.closest('form').submit();"
+                                >
+                                    {{ __('Log Out') }}
+                                </a>
+                            </form>
                         @else
                             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
