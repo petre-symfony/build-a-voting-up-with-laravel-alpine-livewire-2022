423776f659e696adc3d4948bee03c80cff7602b1
petrero <petrero@gmail.com>
2022-11-11 00:20:55 +0200

    4.4 Add a button to the form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,21 @@
                                 placeholder="Describe your idea"
                             ></textarea>
                         </div>
+                        <div class="flex items-center justify-between space-x-3">
+                            <button
+                                type="button"
+                                class="flex items-center justify-center w-1/2 h-11
+                                    text-xs bg-gray-200 font-semibold rounded-xl
+                                    border border-gray-200 hover:border-gray-400
+                                    transition duration-150 ease-in px-6 py-3
+                                "
+                            >
+                                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
+                                </svg>
+                                <span class="ml-2">Attach</span>
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
