ab966d6898b6af207593465912943015fd9f5960
petrero <petrero@gmail.com>
2022-11-10 22:53:34 +0200

    2.19 Search filter

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,5 +17,13 @@
                 <option value="Filter Four">Filter Four</option>
             </select>
         </div>
+
+        <div class="w-2/3 relative">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+
+            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white px-4 py-2 border-none pl-8">
+        </div>
     </div><!-- end filters -->
 </x-app-layout>
