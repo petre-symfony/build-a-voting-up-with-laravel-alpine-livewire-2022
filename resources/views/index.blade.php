4e3fe9124120dd9ad17e981ff21d5ea0386bea23
petrero <petrero@gmail.com>
2022-11-10 23:04:38 +0200

    3.2 Vote button on idea card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,6 +34,9 @@
                     <div class="font-semibold text-2xl">12</div>
                     <div class="text-gray-500">Votes</div>
                 </div>
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 font-bold text-xs uppercase rounded-xl px-4 py-3">Vote</button>
+                </div>
             </div>
         </div><!-- end idea-container -->
     </div><!-- end ideas container -->
