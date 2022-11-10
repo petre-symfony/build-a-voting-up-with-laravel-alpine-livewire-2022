c05681385ffafaab952b2a52697b06ee9d3e319a
petrero <petrero@gmail.com>
2022-11-10 22:05:35 +0200

    2.6 Create login link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,20 @@
     <body class="font-sans text-gray-900 text-sm">
         <header class="flex items-center justify-between px-8 py-4">
             <a href="">Laracasts logo</a>
-            <div>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                    <div class="px-6 py-4">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
                 <a href="">
                     <img
                         src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp"
