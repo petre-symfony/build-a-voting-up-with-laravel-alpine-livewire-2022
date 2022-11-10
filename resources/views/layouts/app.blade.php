c6277e594e085c87e50ba9a217559b9577e20eb5
petrero <petrero@gmail.com>
2022-11-11 00:17:40 +0200

    4.2 Add category select to the form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,14 @@
                                    placeholder="Your Idea"
                             >
                         </div>
+                        <div>
+                            <select name="category_add" id="category_add" class="w-full rounded-xl border-none bg-gray-100 text-sm px-4 py-2">
+                                <option value="Category One">Category One</option>
+                                <option value="Category Two">Category Two</option>
+                                <option value="Category Three">Category Three</option>
+                                <option value="Category Four">Category Four</option>
+                            </select>
+                        </div>
                     </form>
                 </div>
             </div>
