d1993611b163a3a0dbab7fdd7284479842c47430
petrero <petrero@gmail.com>
2022-11-10 22:51:59 +0200

    2.18 Add one more select

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,5 +8,14 @@
                 <option value="Category Four">Category Four</option>
             </select>
         </div>
+
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Filter One">Filter One</option>
+                <option value="Filter Two">Filter Two</option>
+                <option value="Filter Three">Filter Three</option>
+                <option value="Filter Four">Filter Four</option>
+            </select>
+        </div>
     </div><!-- end filters -->
 </x-app-layout>
