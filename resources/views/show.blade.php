d6c686295e8cb14be7b1f8e6b6372558d3145b3e
petrero <petrero@gmail.com>
2022-11-11 01:09:58 +0200

    5.7 Add a comment to the idea

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -18,8 +18,12 @@
                 <h4 class="text-xl font-semibold">
                     <a href="" class="hover:underline">A random title can go here</a>
                 </h4>
-                <div class="text-gray-600 mt-3 line-clamp-3">
-                    Lorem ipsum dolor sit amet
+                <div class="text-gray-600 mt-3">
+                    Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Amet architecto, blanditiis commodi cumque cupiditate
+                    error eveniet ex excepturi expedita incidunt iure iusto natus nihil, non
+                    nostrum nulla odio odit omnis placeat, quasi qui quis quisquam quos saepe
+                    sint ullam unde.
                 </div>
                 <div class="flex items-center justify-between mt-6">
                     <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
@@ -99,4 +103,42 @@
             </button>
         </div>
     </div><!-- end buttons-container -->
+
+    <div class="comments-container space-y-6 ml-22 my-8">
+        <div class="comment-container bg-white rounded-xl flex mt-4">
+            <div class="flex flex-1 px-4 py-6">
+                <div>
+                    <a href="" class="flex-none">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="h-14 w-14 rounded-xl">
+                    </a>
+                </div>
+                <div class="mx-4 w-full">
+                    {{--<h4 class="text-xl font-semibold">
+                        <a href="" class="hover:underline">A random title can go here</a>
+                    </h4> --}}
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div class="font-bold text-gray-900">John Doe</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 border transition duration-150 ease-in rounded-full h-7 py-2 px-4">
+                                <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
+                                <ul class="hidden absolute w-44 text-left ml-8 font-semibold bg-white shadow-dialog rounded-xl py-3">
+                                    <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
+                                    <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- end comment container -->
+    </div><!-- end comments container -->
 </x-app-layout>
