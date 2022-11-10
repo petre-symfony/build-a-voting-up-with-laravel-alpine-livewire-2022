f31220bb7838f54dc9faa7ab06b644c6743f6c46
petrero <petrero@gmail.com>
2022-11-10 22:48:22 +0200

    2.16 Begin filters section. Breeze install tailwind/forms plugin. This adds some extra-styling to the drop-downs so have some good defaults. In this case adds the arrow down icon for us so we don't have to do that.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,12 @@
 <x-app-layout>
-    <div>Index of ideas</div>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category Two">Category Two</option>
+                <option value="Category Three">Category Three</option>
+                <option value="Category Four">Category Four</option>
+            </select>
+        </div>
+    </div><!-- end filters -->
 </x-app-layout>
