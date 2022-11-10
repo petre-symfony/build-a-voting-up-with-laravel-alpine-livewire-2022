521e8e471a59d631a49af624f3cf447bdf0bb66b
petrero <petrero@gmail.com>
2022-11-10 23:00:51 +0200

    2.21 Adjust the text colors in the three filters to resemble to each other

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,11 +20,11 @@
 
         <div class="w-2/3 relative">
             <div class="absolute top-0 flex items-center h-full ml-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
             </div>
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white px-4 py-2 border-none pl-8">
+            <input type="search" placeholder="Find an idea" class="w-full rounded-xl bg-white px-4 py-2 border-none pl-8 placeholder-gray-900">
         </div>
     </div><!-- end filters -->
 </x-app-layout>
