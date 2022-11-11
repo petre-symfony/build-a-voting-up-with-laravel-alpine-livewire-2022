6b3ee5e39232973174ddb0a0a59d7f1c8e54134d
petrero <petrero@gmail.com>
2022-11-11 12:00:44 +0200

    8.13 Stack the image above the tittle on show page under medium size media

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,7 +8,7 @@
         </a>
     </div>
     <div class="idea-container bg-white rounded-xl flex mt-4">
-        <div class="flex flex-1 px-4 py-6">
+        <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
             <div>
                 <a href="" class="flex-none">
                     <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="h-14 w-14 rounded-xl">
