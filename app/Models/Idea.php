265714096fc3021e97a3bd81e8187a0d66a4a7f7
petrero <petrero@gmail.com>
2022-11-11 14:02:50 +0200

    9.11 Idea model and migrations - Wire sluggable package in our idea model

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = [];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function sluggable(): array {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
