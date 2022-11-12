ab2e6f3168ce0cb3971673427e1766f61751be7b
petrero <petrero@gmail.com>
2022-11-12 16:25:26 +0200

    12.5 Test gravatar functionality

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,15 +2,87 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
 
 class UserTest extends TestCase {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example() {
-        $this->assertTrue(true);
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_generate_gravatar_default_image_when_email_found_first_character_a() {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'afakeemail@fakeemail.com'
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+        $this->assertEquals(
+            'https://gravatar.com/avatar/'
+            .md5($user->email)
+            .'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-1.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_default_image_when_email_found_first_character_z() {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => 'zfakeemail@fakeemail.com'
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+        $this->assertEquals(
+            'https://gravatar.com/avatar/'
+            .md5($user->email)
+            .'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-26.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_default_image_when_email_found_first_character_0() {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '0fakeemail@fakeemail.com'
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+        $this->assertEquals(
+            'https://gravatar.com/avatar/'
+            .md5($user->email)
+            .'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-27.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+        $this->assertTrue($response->successful());
+    }
+
+    /** @test */
+    public function user_can_generate_gravatar_default_image_when_email_found_first_character_9() {
+        $user = User::factory()->create([
+            'name' => 'Andre',
+            'email' => '9fakeemail@fakeemail.com'
+        ]);
+
+        $gravatarUrl = $user->getAvatar();
+        $this->assertEquals(
+            'https://gravatar.com/avatar/'
+            .md5($user->email)
+            .'?s=200&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-36.png',
+            $gravatarUrl
+        );
+
+        $response = Http::get($user->getAvatar());
+        $this->assertTrue($response->successful());
     }
 }
