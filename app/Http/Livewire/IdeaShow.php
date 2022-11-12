66ddf9b8f96ee685c6b9484678b34ce7892f8527
petrero <petrero@gmail.com>
2022-11-12 23:13:12 +0200

    17.4 php artisan livewire:make IdeaShow

---
+++ app/Http/Livewire/IdeaShow.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class IdeaShow extends Component {
+    public function render() {
+        return view('livewire.idea-show');
+    }
+}
