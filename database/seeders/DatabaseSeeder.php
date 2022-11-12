72c7e2cb64d8a043ec86ca56c069eef42f2145be
petrero <petrero@gmail.com>
2022-11-12 16:51:09 +0200

    13.3 php artisan migrate:fresh --seed - Seed four categories before run this command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Idea;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,11 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
+        Category::factory()->create(['name' => 'Category 1']);
+        Category::factory()->create(['name' => 'Category 2']);
+        Category::factory()->create(['name' => 'Category 3']);
+        Category::factory()->create(['name' => 'Category 4']);
+
         Idea::factory(30)->create();
     }
 }
