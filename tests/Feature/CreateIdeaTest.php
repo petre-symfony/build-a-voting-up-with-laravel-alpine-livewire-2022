77ad9dbe5af9a50feec8cf73acc6fe6c6d617f5a
petrero <petrero@gmail.com>
2022-11-12 21:23:03 +0200

    16.2 Test the presence of the form when user is logged in and the absence of it when the user is logged out

--- tests/Feature/CreateIdeaTest.php
+++ tests/Feature/CreateIdeaTest.php
@@ -2,19 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CreateIdeaTest extends TestCase {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example() {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function create_ideas_form_does_not_show_when_logged_out() {
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Please login to create an idea');
+        $response->assertDontSee('Let us know what you would like and we\'ll take a look over!');
+    }
+
+    /** @test */
+    public function create_ideas_form_does_show_when_logged_in() {
+        $response = $this->actingAs(User::factory()->create())->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertDontSee('Please login to create an idea');
+        $response->assertSee('Let us know what you would like and we\'ll take a look over!', false);
     }
 }
