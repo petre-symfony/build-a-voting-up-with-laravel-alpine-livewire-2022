65a426b8c7385c0add4a6f94c6f88c1bff25b45e
petrero <petrero@gmail.com>
2022-11-12 20:22:52 +0200

    15.2 php artisan make:livewire createIdea

---
+++ app/Http/Livewire/CreateIdea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CreateIdea extends Component {
+    public function render() {
+        return view('livewire.create-idea');
+    }
+}
