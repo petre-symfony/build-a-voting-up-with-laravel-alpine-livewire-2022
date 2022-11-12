d6a00bbfdbdc86c66a53f40521e5248b98a2e4d5
petrero <petrero@gmail.com>
2022-11-12 21:19:59 +0200

    16.1 Create a test for testing creating ideas and correct a previous test in ShowIdeasTest

--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -80,12 +80,12 @@ class ShowIdeasTest extends TestCase {
         $ideaEleven->save();
 
         $response = $this->get('/');
-        $response->assertSee($ideaOne->title);
-        $response->assertDontSee($ideaEleven->title);
-
-        $response = $this->get('/?page=2');
         $response->assertDontSee($ideaOne->title);
         $response->assertSee($ideaEleven->title);
+
+        $response = $this->get('/?page=2');
+        $response->assertSee($ideaOne->title);
+        $response->assertDontSee($ideaEleven->title);
     }
 
     /** @test */
