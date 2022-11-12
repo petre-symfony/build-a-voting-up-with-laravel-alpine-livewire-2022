eb3e922c6f5a3f4c5e0bd552cd28dffe34214a64
petrero <petrero@gmail.com>
2022-11-12 23:02:52 +0200

    17.2 Tailor the migration and run php artisan migrate:fresh --seed to apply the migration and seed our database

--- database/migrations/2022_10_30_153740_create_votes_table.php
+++ database/migrations/2022_10_30_153740_create_votes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration {
     public function up() {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
+            $table->unique(['idea_id', 'user_id']);
+            $table->foreignId('idea_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
