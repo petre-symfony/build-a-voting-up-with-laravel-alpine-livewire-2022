1bd98a80deb3bfce9223c1147c3e46eea6e1840f
petrero <petrero@gmail.com>
2022-11-13 21:03:57 +0200

    26.4 Test the presence of the StatusFilter livewire component on show and index page

--- tests/Feature/StatusFiltersTest.php
+++ tests/Feature/StatusFiltersTest.php
@@ -2,19 +2,54 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StatusFiltersTest extends TestCase {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example() {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function index_page_contains_status_filters_livewire_component(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        $this->get(route('idea.index'))
+            ->assertSeeLivewire('status-filters');
+    }
+
+    /** @test */
+    public function show_page_contains_status_filters_livewire_component(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('status-filters');
     }
 }
