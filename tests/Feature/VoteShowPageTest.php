594d2e91182c7ce120fed5cd778bff83522cc06b
petrero <petrero@gmail.com>
2022-11-13 11:12:11 +0200

    18.2 Test the presence of idea-show livewire component

--- tests/Feature/VoteShowPageTest.php
+++ tests/Feature/VoteShowPageTest.php
@@ -2,19 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class VoteShowPageTest extends TestCase {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example() {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function show_page_contains_idea_show_livewire_component(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('idea-show');
     }
 }
