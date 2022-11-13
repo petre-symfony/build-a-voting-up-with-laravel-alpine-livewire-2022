f39305caad2158e67421461f605fd70488570e97
petrero <petrero@gmail.com>
2022-11-13 17:40:39 +0200

    23.1 php artisan livewire:make IdeasIndex

---
+++ app/Http/Livewire/IdeasIndex.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class IdeasIndex extends Component {
+    public function render() {
+        return view('livewire.ideas-index');
+    }
+}
