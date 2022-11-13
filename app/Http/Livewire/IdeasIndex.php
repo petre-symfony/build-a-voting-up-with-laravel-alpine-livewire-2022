eb083511c30ebf1536cb487397a5e1659973f302
petrero <petrero@gmail.com>
2022-11-13 21:42:04 +0200

    27.5 Filter the ideas based on their statuses and/or categories

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -37,11 +37,15 @@ class IdeasIndex extends Component {
 
     public function render() {
         $statuses = Status::all()->pluck('id', 'name');
+        $categories = Category::all();
 
         return view('livewire.ideas-index', [
             'ideas' => Idea::with('user', 'category', 'status')
                 ->when($this->status && $this->status !== 'All', function($query) use ($statuses){
                     return $query->where('status_id', $statuses->get($this->status));
+                })
+                ->when($this->category && $this->category !== 'All Categories', function($query) use ($categories){
+                    return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
                 })
                 ->withCount('votes')
                 ->addSelect(['voted_by_user' => Vote::select('id')
@@ -50,7 +54,7 @@ class IdeasIndex extends Component {
                 ])
                 ->orderBy('id', 'desc')
                 ->simplePaginate(Idea::PAGINATION_COUNT),
-            'categories' => Category::all()
+            'categories' => $categories
         ]);
     }
 }
