e7fb56acfb06783a91b0d8cec8fafef71e3959ec
petrero <petrero@gmail.com>
2022-11-13 19:37:38 +0200

    24.7 Doesn't perform a full page refresh if we're already on index page

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -22,12 +22,18 @@ class StatusFilters extends Component {
     public function setStatus($newStatus){
         $this->status = $newStatus;
 
-        return $this->redirect(route('idea.index', [
-            'status' => $this->status
-        ]));
+        if ($this->getPreviousRouteName() === 'idea.show') {
+            return $this->redirect(route('idea.index', [
+                'status' => $this->status
+            ]));
+        }
     }
 
     public function render() {
         return view('livewire.status-filters');
+    }
+
+    private function getPreviousRouteName() {
+        return app('router')->getRoutes()->match(app('request')->create(url()->previous()))->getName();
     }
 }
