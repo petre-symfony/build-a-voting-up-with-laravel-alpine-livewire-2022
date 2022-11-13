22fd5abf7d5877c77fbdaa258a0ec543f2c510e0
petrero <petrero@gmail.com>
2022-11-13 11:20:44 +0200

    18.7 Test the behaviour of the idea-index livewire component

--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\IdeaIndex;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -9,6 +10,7 @@ use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class VoteIndexPageTest extends TestCase {
@@ -64,5 +66,30 @@ class VoteIndexPageTest extends TestCase {
             ->assertViewHas('ideas', function($ideas){
                 return $ideas->first()->votes_count == 2;
             });
+    }
+
+    /** @test */
+    public function votes_count_shows_correctly_on_index_page_livewire_component(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Livewire::test(IdeaIndex::class, [
+            'idea' => $idea,
+            'votesCount' => 5
+        ])
+            ->assertSet('votesCount', 5)
+            ->assertSeeHtml('<div class="font-semibold text-2xl">5</div>')
+            ->assertSeeHtml('<div class="text-sm font-bold leading-none">5</div>');
     }
 }
