934dfd39995a349cda5610eb7cd47c81fc75f484
petrero <petrero@gmail.com>
2022-11-13 18:23:04 +0200

    24.2 Load status filters through the livewire statusFilters component. We want a piece of state that's put in a query and is updated by the query

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -5,6 +5,12 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 class StatusFilters extends Component {
+    public $status = 'All';
+
+    protected $queryString = [
+        'status'
+    ];
+
     public function render() {
         return view('livewire.status-filters');
     }
