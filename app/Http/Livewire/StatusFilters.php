18ebccbf6e4a46f92151227630415bc594af0095
petrero <petrero@gmail.com>
2022-11-13 18:12:03 +0200

    24.1 php artisan make:livewire statusFilters

---
+++ app/Http/Livewire/StatusFilters.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StatusFilters extends Component {
+    public function render() {
+        return view('livewire.status-filters');
+    }
+}
