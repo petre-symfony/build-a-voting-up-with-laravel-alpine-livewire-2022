322ecdc92757db57b2b7e370ad8af08cee7b5af1
petrero <petrero@gmail.com>
2022-11-13 11:16:12 +0200

    18.4 Test that idea-show livewire component receives votesCount variable correctly

--- tests/Feature/VoteShowPageTest.php
+++ tests/Feature/VoteShowPageTest.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\User;
+use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -31,5 +32,35 @@ class VoteShowPageTest extends TestCase {
 
         $this->get(route('idea.show', $idea))
             ->assertSeeLivewire('idea-show');
+    }
+
+    /** @test */
+    public function show_page_correctly_receives_votes_count(){
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id
+        ]);
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $userB->id
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertViewHas('votesCount', 2);
     }
 }
