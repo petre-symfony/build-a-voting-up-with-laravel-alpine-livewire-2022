af0ee5aaba0ff1dee73511647749c2731f22b9f5
petrero <petrero@gmail.com>
2022-11-13 11:17:53 +0200

    18.5 Test that idea-index livewire component has ideas containers that have votes_count parameter set correctly

--- tests/Feature/VoteIndexPageTest.php
+++ tests/Feature/VoteIndexPageTest.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\User;
+use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -31,5 +32,37 @@ class VoteIndexPageTest extends TestCase {
 
         $this->get(route('idea.index'))
             ->assertSeeLivewire('idea-index');
+    }
+
+    /** @test */
+    public function index_page_correctly_receives_votes_count(){
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id
+        ]);
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $userB->id
+        ]);
+
+        $this->get(route('idea.index'))
+            ->assertViewHas('ideas', function($ideas){
+                return $ideas->first()->votes_count == 2;
+            });
     }
 }
