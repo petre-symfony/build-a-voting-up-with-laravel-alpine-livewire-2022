b2667a7c632bfb0b021847c116b5da5ea2aed622
petrero <petrero@gmail.com>
2022-11-13 21:06:21 +0200

    26.5 Check if the idea counting is correctly displayed on page

--- tests/Feature/StatusFiltersTest.php
+++ tests/Feature/StatusFiltersTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\IdeasIndex;
+use App\Http\Livewire\StatusFilters;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class StatusFiltersTest extends TestCase {
@@ -51,5 +53,34 @@ class StatusFiltersTest extends TestCase {
 
         $this->get(route('idea.show', $idea))
             ->assertSeeLivewire('status-filters');
+    }
+
+    /** @test */
+    public function shows_correct_status_count(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusImplemented = Status::factory()->create(['id' => 4, 'name' => 'Implemented']);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusImplemented->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusImplemented->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Livewire::test(StatusFilters::class)
+            ->assertSee('All Ideas (2)')
+            ->assertSee('Implemented (2)');
     }
 }
