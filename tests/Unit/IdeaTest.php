87e93c542a075327bb02c5271dcfd93f7995e8e8
petrero <petrero@gmail.com>
2022-11-13 17:02:33 +0200

    20.5 Write a test for vote functionality

--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -106,4 +106,25 @@ class IdeaTest extends TestCase {
             ->assertSet('hasVoted', true)
             ->assertSee('Voted');
     }
+
+    /** @test */
+    public function user_can_vote_for_idea(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        $this->assertFalse($idea->isVotedByUser($user));
+        $idea->vote($user);
+        $this->assertTrue($idea->isVotedByUser($user));
+    }
 }
