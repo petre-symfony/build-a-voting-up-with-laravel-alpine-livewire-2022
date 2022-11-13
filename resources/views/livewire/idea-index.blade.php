b1189d427a1f6043acf8ccbf7ab69cc618dafd3e
petrero <petrero@gmail.com>
2022-11-13 12:11:34 +0200

    19.9 The vote button under the votesCount text change its text to voted, its color to blue and its text color to white if that idea is voted by the logged in user

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -18,14 +18,23 @@
             <div class="text-gray-500">Votes</div>
         </div>
         <div class="mt-8">
-            <button
-                class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400
-                            transition duration-150 ease-in
-                            font-bold text-xs uppercase rounded-xl px-4 py-3
-                        "
-            >
-                Vote
-            </button>
+            @if ($hasVoted)
+                <button
+                    class="w-20 bg-blue text-white border border-blue hover:bg-blue-hover
+                        transition duration-150 ease-in font-bold text-xxs uppercase
+                        rounded-xl px-4 py-3
+                    "
+                >Voted</button>
+            @else
+                <button
+                    class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400
+                        transition duration-150 ease-in
+                        font-bold text-xs uppercase rounded-xl px-4 py-3
+                    "
+                >
+                    Vote
+                </button>
+            @endif
         </div>
     </div>
     <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
@@ -93,12 +102,21 @@
                         <div class="text-sm font-bold leading-none @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                         <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                     </div>
-                    <button
-                        class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400
-                                    transition duration-150 ease-in font-bold text-xxs text-white uppercase
-                                    rounded-xl px-4 py-3 -mx-5
-                                "
-                    >Vote</button>
+                    @if ($hasVoted)
+                        <button
+                            class="w-20 bg-blue text-white border border-blue hover:bg-blue-hover
+                                transition duration-150 ease-in font-bold text-xxs text-white uppercase
+                                rounded-xl px-4 py-3 -mx-5
+                            "
+                        >Voted</button>
+                    @else
+                        <button
+                            class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400
+                                transition duration-150 ease-in font-bold text-xxs text-white uppercase
+                                rounded-xl px-4 py-3 -mx-5
+                            "
+                        >Vote</button>
+                    @endif
                 </div>
             </div>
         </div>
