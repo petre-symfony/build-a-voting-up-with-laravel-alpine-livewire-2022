a3463d765f001cc31711f50cf0db4382bc980a6f
petrero <petrero@gmail.com>
2022-11-13 11:19:38 +0200

    18.6 Test the behaviour of the idea-show livewire component

--- tests/Feature/VoteShowPageTest.php
+++ tests/Feature/VoteShowPageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\IdeaShow;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
@@ -9,6 +10,7 @@ use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class VoteShowPageTest extends TestCase {
@@ -62,5 +64,30 @@ class VoteShowPageTest extends TestCase {
 
         $this->get(route('idea.show', $idea))
             ->assertViewHas('votesCount', 2);
+    }
+
+    /** @test */
+    public function votes_count_shows_correctly_on_show_page_livewire_component(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Livewire::test(IdeaShow::class, [
+            'idea' => $idea,
+            'votesCount' => 5
+        ])
+            ->assertSet('votesCount', 5)
+            ->assertSeeHtml('<div class="text-xl leading-snug">5</div>')
+            ->assertSeeHtml('<div class="text-sm font-bold leading-none">5</div>');
     }
 }
