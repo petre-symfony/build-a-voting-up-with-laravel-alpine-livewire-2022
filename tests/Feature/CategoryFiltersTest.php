13e9f532d1243df6f1b5852b8864fd70b6721f26
petrero <petrero@gmail.com>
2022-11-13 21:53:48 +0200

    28.3 Test the query string corectness

--- tests/Feature/CategoryFiltersTest.php
+++ tests/Feature/CategoryFiltersTest.php
@@ -54,4 +54,45 @@ class CategoryFiltersTest extends TestCase {
                     && $ideas->first()->category->name == 'Category 1';
             });
     }
+
+    /** @test */
+    public function the_category_query_string_filters_correctly(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categorTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $statusOpen = Status::factory()->create(['id' => 4, 'name' => 'Open']);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categorTwo->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Livewire::withQueryParams(['category' => 'Category 1'])
+            ->test(IdeasIndex::class)
+            ->assertViewHas('ideas', function($ideas){
+                return $ideas->count() == 2
+                    && $ideas->first()->category->name == 'Category 1';
+            });
+    }
 }
