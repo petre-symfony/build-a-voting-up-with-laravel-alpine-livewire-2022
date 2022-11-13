3563bb91cb69bac11418e05d3d4aa7eaebf8f5a0
petrero <petrero@gmail.com>
2022-11-13 11:05:22 +0200

    17.11 Each idea container in the home page is displayed by using a livewire component

--- app/Http/Livewire/IdeaIndex.php
+++ app/Http/Livewire/IdeaIndex.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class IdeaIndex extends Component {
+    public $idea;
+    public $votesCount;
+
+    public function mount(Idea $idea, $votesCount) {
+        $this->idea = $idea;
+        $this->votesCount = $votesCount;
+    }
+
     public function render() {
         return view('livewire.idea-index');
     }
