628aaf753db987c3dfedabe34a73833e42e46c27
petrero <petrero@gmail.com>
2022-11-13 20:53:01 +0200

    25.3 Maintain the query string as we click on pagination links

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -36,5 +36,8 @@
             />
         @endforeach
     </div><!-- end ideas container -->
-    <div>{{ $ideas->links() }}</div>
+    <div>
+        {{-- {{ $ideas->links() }} --}}
+        {{ $ideas->appends(request()->query())->links() }}
+    </div>
 </div>
