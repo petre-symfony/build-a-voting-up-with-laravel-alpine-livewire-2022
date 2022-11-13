3899b31d32239950ef88644c9505ae1011ea7f65
petrero <petrero@gmail.com>
2022-11-13 12:13:36 +0200

    19.10 Test the vote functionality we added in show page

--- tests/Unit/IdeaTest.php
+++ tests/Unit/IdeaTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Unit;
 
+use App\Http\Livewire\IdeaShow;
 use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\User;
 use App\Models\Vote;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class IdeaTest extends TestCase {
@@ -38,5 +40,35 @@ class IdeaTest extends TestCase {
         $this->assertTrue($idea->isVotedByUser($user));
         $this->assertFalse($idea->isVotedByUser($userB));
         $this->assertFalse($idea->isVotedByUser(null));
+    }
+
+    /** @test */
+    public function user_who_is_logged_in_shows_voted_if_idea_already_voted_for(){
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
+            'title' => 'My First Idea',
+            'description' => 'Description for my first idea'
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(IdeaShow::class, [
+                'idea' => $idea,
+                'votesCount' => 5
+            ])
+            ->assertSet('hasVoted', true)
+            ->assertSee('Voted');
     }
 }
