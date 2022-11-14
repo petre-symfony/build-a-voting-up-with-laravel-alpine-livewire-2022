60e90dbfe3af7185faebe7eed9bb0a97529a5058
petrero <petrero@gmail.com>
2022-11-14 11:12:15 +0200

    33.5 Write test that assert the absence of set-status livewire component when the logged in user is not an admin

--- tests/Feature/AdminSetStatusTest.php
+++ tests/Feature/AdminSetStatusTest.php
@@ -32,4 +32,25 @@ class AdminSetStatusTest extends TestCase {
             ->get(route('idea.show', $idea))
             ->assertSeeLivewire('set-status');
     }
+
+    /** @test */
+    public function show_page_does_not_contain_set_status_livewire_component_when_user_is_not_admin() {
+        $user = User::factory()->create([
+            'email' => 'andre@hotmail.com'
+        ]);
+
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id
+        ]);
+
+        $this->actingAs($user)
+            ->get(route('idea.show', $idea))
+            ->assertDontSeeLivewire('set-status');
+    }
 }
