488120eb01e0ff6ffeb1fb033bb1480a6c604068
petrero <petrero@gmail.com>
2022-11-14 09:22:51 +0200

    31.1 Remeber the filters we activated on index page when go back to index page after we visited show page

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -45,7 +45,10 @@ class IdeaController extends Controller {
     public function show(Idea $idea) {
         return view('idea.show', [
             'idea' => $idea,
-            'votesCount' => $idea->votes()->count()
+            'votesCount' => $idea->votes()->count(),
+            'backUrl' => url()->previous() !== url()->full()
+                ? url()->previous()
+                : route('idea.index')
         ]);
     }
 
