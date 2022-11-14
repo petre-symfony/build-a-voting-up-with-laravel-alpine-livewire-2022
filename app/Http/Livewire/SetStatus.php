e11fec564b0a297b3c24d642b768e4688acb1e04
petrero <petrero@gmail.com>
2022-11-14 09:47:39 +0200

    32.2 Move setStatus button and its modal into its own livewire component

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class SetStatus extends Component {
+    public $idea;
+
+    public function mount(Idea $idea) {
+        $this->idea = $idea;
+    }
+
     public function render() {
         return view('livewire.set-status');
     }
