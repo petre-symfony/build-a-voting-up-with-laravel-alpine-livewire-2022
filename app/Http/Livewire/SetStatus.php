698ecf6be27221f458bf9803dde3042ad9207a9b
petrero <petrero@gmail.com>
2022-11-14 10:26:52 +0200

    32.4 The correct status is now checked when open the set status modal.

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -3,16 +3,21 @@
 namespace App\Http\Livewire;
 
 use App\Models\Idea;
+use App\Models\Status;
 use Livewire\Component;
 
 class SetStatus extends Component {
     public $idea;
+    public $status;
 
     public function mount(Idea $idea) {
         $this->idea = $idea;
+        $this->status = $this->idea->status_id;
     }
 
     public function render() {
-        return view('livewire.set-status');
+        return view('livewire.set-status', [
+            'statuses' => Status::all()
+        ]);
     }
 }
