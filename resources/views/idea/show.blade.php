d623792ea17a7aadbf82658ccf912bf57deab905
petrero <petrero@gmail.com>
2022-11-15 11:51:37 +0200

    37.10 The same idea from the previous commit for three dot buttons in comments section

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -37,16 +37,19 @@
                             x-data = "{ isOpen: false}"
                             class="flex items-center space-x-2"
                         >
-                            <button
-                                @click="isOpen = !isOpen"
-                                class="
-                                    relative bg-gray-100 hover:bg-gray-200 border transition
-                                    duration-150 ease-in rounded-full h-7 py-2 px-4
-                                "
-                            >
-                                <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                                </svg>
+                            <div class="relative">
+                                <button
+                                    @click="isOpen = !isOpen"
+                                    class="
+                                        relative bg-gray-100 hover:bg-gray-200 border transition
+                                        duration-150 ease-in rounded-full h-7 py-2 px-4
+                                    "
+                                >
+                                    <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                    </svg>
+
+                                </button>
                                 <ul
                                     x-cloak
                                     x-show="isOpen"
@@ -62,7 +65,7 @@
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
                                 </ul>
-                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -93,16 +96,18 @@
                             x-data = "{ isOpen: false}"
                             class="flex items-center space-x-2"
                         >
-                            <button
-                                @click="isOpen = !isOpen"
-                                class="
-                                    relative bg-gray-100 hover:bg-gray-200 border transition
-                                    duration-150 ease-in rounded-full h-7 py-2 px-4
-                                "
-                            >
-                                <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                                </svg>
+                            <div class="relative">
+                                <button
+                                    @click="isOpen = !isOpen"
+                                    class="
+                                        relative bg-gray-100 hover:bg-gray-200 border transition
+                                        duration-150 ease-in rounded-full h-7 py-2 px-4
+                                    "
+                                >
+                                    <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                    </svg>
+                                </button>
                                 <ul
                                     x-cloak
                                     x-show="isOpen"
@@ -117,7 +122,7 @@
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
                                 </ul>
-                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -147,16 +152,18 @@
                             x-data = "{ isOpen: false}"
                             class="flex items-center space-x-2"
                         >
-                            <button
-                                @click="isOpen = !isOpen"
-                                class="
-                                    relative bg-gray-100 hover:bg-gray-200 border transition
-                                    duration-150 ease-in rounded-full h-7 py-2 px-4
-                                "
-                            >
-                                <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                                </svg>
+                            <div class="relative">
+                                <button
+                                    @click="isOpen = !isOpen"
+                                    class="
+                                        relative bg-gray-100 hover:bg-gray-200 border transition
+                                        duration-150 ease-in rounded-full h-7 py-2 px-4
+                                    "
+                                >
+                                    <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                    </svg>
+                                </button>
                                 <ul
                                     x-cloak
                                     x-show="isOpen"
@@ -171,7 +178,7 @@
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
                                     <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
                                 </ul>
-                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
