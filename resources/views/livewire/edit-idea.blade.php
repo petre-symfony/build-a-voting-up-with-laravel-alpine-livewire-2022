8985ae639b2200b1d8db6ffff4ced473aea0baaf
petrero <petrero@gmail.com>
2022-11-15 10:52:23 +0200

    37.5 Copy the form from create idea livewire template to edit idea livewire template and make minor adjustments

--- resources/views/livewire/edit-idea.blade.php
+++ resources/views/livewire/edit-idea.blade.php
@@ -25,7 +25,66 @@
             -->
             <div class="modal relative transform overflow-hidden rounded-tl-xl rounded-tr-xl bg-white transition-all py-4 sm:w-full sm:max-w-lg">
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L
+                    <form action="" wire:submit.prevent="createIdea" method="POST" class="space-y-4 px-4 py-6">
+                        <div>
+                            <input type="text" wire:model.defer="title"
+                               class="w-full bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2 border-none text-sm"
+                               placeholder="Your Idea"
+                            >
+                            @error('title')
+                            <p class="text-red text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <select
+                                name="category_add" id="category_add"
+                                class="w-full rounded-xl border-none bg-gray-100 text-sm px-4 py-2"
+                                wire:model.defer="category"
+                            >
+                                <option value="1">Category 1</option>
+                            </select>
+                            @error('category')
+                            <p class="text-red text-xs mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div>
+                        <textarea
+                            name="idea" id="idea" cols="30" rows="4"
+                            class="w-full bg-gray-100 rounded-xl placeholder-gray-900 text-sm px-4 py-2 border-none"
+                            placeholder="Describe your idea"
+                            wire:model.defer="description"
+                        ></textarea>
+                        @error('description')
+                        <p class="text-red text-xs mt-1">{{ $message }}</p>
+                        @enderror
+                        </div>
+                        <div class="flex items-center justify-between space-x-3">
+                            <button
+                                type="button"
+                                class="flex items-center justify-center w-1/2 h-11
+                                    text-xs bg-gray-200 font-semibold rounded-xl
+                                    border border-gray-200 hover:border-gray-400
+                                    transition duration-150 ease-in px-6 py-3
+                                "
+                            >
+                                <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
+                                </svg>
+                                <span class="ml-1">Attach</span>
+                            </button>
+
+                            <button
+                                type="submit"
+                                class="flex items-center justify-center w-1/2 h-11
+                                    text-xs text-white bg-blue font-semibold rounded-xl
+                                    border border-blue hover:bg-blue-hover
+                                    transition duration-150 ease-in px-6 py-3
+                                "
+                            >
+                                <span class="ml-1">Submit</span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
