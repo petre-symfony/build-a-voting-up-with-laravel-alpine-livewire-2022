bc271c7788f9759a814a59b80457c1616c8599ad
petrero <petrero@gmail.com>
2022-11-16 11:45:54 +0200

    40.4 Test validation for edit idea form

--- tests/Feature/EditIdeaTest.php
+++ tests/Feature/EditIdeaTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\CreateIdea;
 use App\Http\Livewire\EditIdea;
 use App\Models\Idea;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class EditIdeaTest extends TestCase {
@@ -26,12 +28,29 @@ class EditIdeaTest extends TestCase {
     /** @test */
     public function does_not_show_edit_idea_livewire_component_when_user_does_not_have_authorization() {
         $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('idea.show', $idea))
+            ->assertDontSeeLivewire('edit-idea');
+    }
+
+    /** @test */
+    public function edit_idea_form_validation_works() {
+        $user = User::factory()->create();
         $idea = Idea::factory()->create([
             'user_id' => $user->id
         ]);
 
-        $this->actingAs($user)
-            ->get(route('idea.show', $idea))
-            ->assertSeeLivewire('edit-idea');
+        Livewire::actingAs($user)
+            ->test(EditIdea::class, [
+                'idea' => $idea
+            ])
+            ->set('title', '')
+            ->set('category', '')
+            ->set('description', '')
+            ->call('updateIdea')
+            ->assertHasErrors(['title', 'category', 'description'])
+            ->assertSee('The title field is required');
     }
 }
