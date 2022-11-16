51d467878c555ec06ac6e6dc707993e70c501b98
petrero <petrero@gmail.com>
2022-11-16 12:28:08 +0200

    41.1 php artisan livewire:make DeleteIdea

---
+++ app/Http/Livewire/DeleteIdea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DeleteIdea extends Component {
+    public function render() {
+        return view('livewire.delete-idea');
+    }
+}
