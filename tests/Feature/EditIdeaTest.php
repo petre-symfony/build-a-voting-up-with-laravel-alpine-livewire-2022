dcb7f1676277fff8318df6c4364f32d3b35f3c10
petrero <petrero@gmail.com>
2022-11-16 11:57:20 +0200

    40.5 Test that an authorized user can update an idea

--- tests/Feature/EditIdeaTest.php
+++ tests/Feature/EditIdeaTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Http\Livewire\CreateIdea;
 use App\Http\Livewire\EditIdea;
+use App\Models\Category;
 use App\Models\Idea;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -52,5 +53,33 @@ class EditIdeaTest extends TestCase {
             ->call('updateIdea')
             ->assertHasErrors(['title', 'category', 'description'])
             ->assertSee('The title field is required');
+    }
+
+    /** @test */
+    public function editing_an_idea_works_when_user_has_authorization() {
+        $user = User::factory()->create();
+        $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $categoryOne->id
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(EditIdea::class, [
+                'idea' => $idea
+            ])
+            ->set('title', 'My Edited Idea')
+            ->set('category', $categoryTwo->id)
+            ->set('description', 'This is my edited idea')
+            ->call('updateIdea')
+            ->assertEmitted('ideaWasUpdated');
+
+        $this->assertDatabaseHas('ideas', [
+            'category_id' => $categoryTwo->id,
+            'title' => 'My Edited Idea',
+            'description' => 'This is my edited idea'
+        ]);
     }
 }
