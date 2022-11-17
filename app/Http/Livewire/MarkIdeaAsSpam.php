2be3f03cef245ed4fc0e4298ed6077860d89a2dc
petrero <petrero@gmail.com>
2022-11-17 11:00:03 +0200

    43.1 php artisan livewire:make MarkIdeaAsSpam

---
+++ app/Http/Livewire/MarkIdeaAsSpam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MarkIdeaAsSpam extends Component {
+    public function render() {
+        return view('livewire.mark-idea-as-spam');
+    }
+}
