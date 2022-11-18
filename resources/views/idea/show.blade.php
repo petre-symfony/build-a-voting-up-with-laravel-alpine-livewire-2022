7f31ab445c79536da11c14ede2b5be69d370fab9
petrero <petrero@gmail.com>
2022-11-18 12:35:23 +0200

    46.9 Encompass the overlay notification with another div in order to add a button that toggles the visibility of notification message on or off

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -10,40 +10,43 @@
 
     <livewire:idea-show :idea="$idea" :votesCount="$votesCount"/>
 
-    <div
-        x-cloak
-        x-data="{ isOpen: false }"
-        x-show="isOpen"
-        @keydown.escape.window="isOpen = false"
-        x-init="
-            window.livewire.on('ideaWasUpdated', () => {
-                isOpen = false
-            })
-        "
-        class="
-            flex justify-between max-w-xs sm:max-w-sm w-full
-            fixed bottom-0 right-0 bg-white rounded-xl shadow-lg border
-            px-6 py-5 mx-6 my-8 z-20
-        "
-    >
+    <div x-data="{ isOpen: false }">
+        <button @click="isOpen = !isOpen">Toggle</button>
         <div
+            x-cloak
+
+            x-show="isOpen"
+            @keydown.escape.window="isOpen = false"
+            x-init="
+                window.livewire.on('ideaWasUpdated', () => {
+                    isOpen = false
+                })
+            "
             class="
-                flex items-center
+                flex justify-between max-w-xs sm:max-w-sm w-full
+                fixed bottom-0 right-0 bg-white rounded-xl shadow-lg border
+                px-6 py-5 mx-6 my-8 z-20
             "
         >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
-            </svg>
+            <div
+                class="
+                    flex items-center
+                "
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                </svg>
 
 
-            <div class="font-semibold text-gray-500 text-sm sm:text-base ml-2">Ideea was updated successfully</div>
+                <div class="font-semibold text-gray-500 text-sm sm:text-base ml-2">Ideea was updated successfully</div>
+            </div>
+            <button class="text-gray-400 hover:text-gray-500">
+                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
+
         </div>
-        <button class="text-gray-400 hover:text-gray-500">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-
     </div>
 
     <x-modals-container :idea="$idea"/>
