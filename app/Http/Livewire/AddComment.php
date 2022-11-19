73196ece7526f7ee6409677dec56a07e52087ee2
petrero <petrero@gmail.com>
2022-11-19 20:10:57 +0200

    49.2 Move html code that displays Reply button and the form asociated with this into ad-comment livewire component

--- app/Http/Livewire/AddComment.php
+++ app/Http/Livewire/AddComment.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
 use Livewire\Component;
 
 class AddComment extends Component {
+    public $idea;
+
+    public function mount(Idea $idea){
+        $this->idea = $idea;
+    }
+    
     public function render() {
         return view('livewire.add-comment');
     }
--- resources/views/livewire/add-comment.blade.php
+++ resources/views/livewire/add-comment.blade.php
@@ -1,3 +1,63 @@
-<div>
-    {{-- Be like water. --}}
+<div
+    x-data = "{ isOpen: false}"
+    class="relative"
+>
+    <button
+        @click="isOpen = !isOpen"
+        type="button"
+        class="flex items-center justify-center h-11 w-32
+            text-xs text-white bg-blue font-semibold rounded-xl
+            border border-blue hover:bg-blue-hover
+            transition duration-150 ease-in px-6 py-3
+        "
+    >
+        <span class="ml-1">Reply</span>
+    </button>
+    <div
+        x-cloak
+        x-show="isOpen"
+        x-transition.origin.top.left.duration.150ms
+        @click.away="isOpen = false"
+        @keydown.escape.window="isOpen = false"
+        class="
+            absolute z-10 w-64 md:w-104 text-left font-semibold text-sm bg-white
+            shadow-dialog rounded-xl mt-5
+        "
+    >
+        <form action="" class="space-y-4 px-4 py-6">
+            <div>
+                <textarea
+                    name="post_comment" id="post_comment" cols="10" rows="4"
+                    class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
+                    placeholder="Go ahead, don't be shy, share your thoughts..."
+                ></textarea>
+
+                <div class="flex flex-col md:flex-row items-center md:space-x-3">
+                    <button
+                        type="button"
+                        class="flex items-center justify-center h-11 w-full md:w-1/2
+                            text-sm text-white bg-blue font-semibold rounded-xl
+                            border border-blue hover:bg-blue-hover
+                            transition duration-150 ease-in px-6 py-3
+                        "
+                    >
+                        Post Comment
+                    </button>
+                    <button
+                        type="button"
+                        class="flex items-center justify-center w-full md:w-32 h-11
+                            text-xs bg-gray-200 font-semibold rounded-xl
+                            border border-gray-200 hover:border-gray-400
+                            transition duration-150 ease-in px-6 py-3 mt-2 md:mt-0
+                        "
+                    >
+                        <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13" />
+                        </svg>
+                        <span class="ml-1">Attach</span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
