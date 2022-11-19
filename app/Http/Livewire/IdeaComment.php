51215c84b4b5f3a63851432f9d89f93a4d0c06f9
petrero <petrero@gmail.com>
2022-11-19 18:43:01 +0200

    48.8 Use idea-comment livewire component to display a single comment for an idea

--- app/Http/Livewire/IdeaComment.php
+++ app/Http/Livewire/IdeaComment.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use Livewire\Component;
 
 class IdeaComment extends Component {
+    public $comment;
+
+    public function mount(Comment $comment){
+        $this->comment = $comment;
+    }
     public function render() {
-        return view('livewire.idea-comment');
+        return view('livewire.idea-comment', [
+            'comment' => $this->comment
+        ]);
     }
 }
