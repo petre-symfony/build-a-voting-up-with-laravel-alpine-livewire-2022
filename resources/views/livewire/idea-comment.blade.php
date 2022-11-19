417e6943fd582f85e60185d937ee2e14f80311a6
petrero <petrero@gmail.com>
2022-11-19 21:39:07 +0200

    49.8 Add a badge after the name of the user

--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -16,6 +16,8 @@
                 <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                     <div class="font-bold text-gray-900">{{ $comment->user->name }}</div>
                     <div>&bull;</div>
+                    <div class="rounded-full bg-gray-100 border px-3 py-1">OP</div>
+                    <div>&bull;</div>
                     <div>{{ $comment->created_at->diffForHumans() }}</div>
                 </div>
                 <div
