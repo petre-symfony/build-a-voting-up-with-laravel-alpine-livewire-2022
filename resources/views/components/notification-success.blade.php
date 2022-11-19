9d34c3ddbde3c1cfa7399f5992e287029ba41f43
petrero <petrero@gmail.com>
2022-11-19 10:12:43 +0200

    47.2 Eliminate the uppermost div in notification success overlay. Put x-init and x-data declaration in the second uppermost div, remaining with one root only. Eliminate the toggle button too. We don't need it anymore

--- resources/views/components/notification-success.blade.php
+++ resources/views/components/notification-success.blade.php
@@ -8,47 +8,36 @@
             }, 5000)
         })
     "
+    x-cloak
+    x-show="isOpen"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 translate-x-8"
+    x-transition:enter-end="opacity-100 translate-x-0"
+    x-transition:leave="transition ease-in duration-150"
+    x-transition:leave-start="opacity-100 translate-x-0"
+    x-transition:leave-end="opacity-0 translate-x-8"
+    @keydown.escape.window="isOpen = false"
+
+    class="
+        flex justify-between max-w-xs sm:max-w-sm w-full
+        fixed bottom-0 right-0 bg-white rounded-xl shadow-lg border
+        px-4 py-5 mx-6 my-8 z-20
+    "
 >
-    <button
-        @click="
-            isOpen = true
-            setTimeout(() => {
-                isOpen = false
-            }, 5000)
-        "
-    >Toggle</button>
     <div
-        x-cloak
-        x-show="isOpen"
-        x-transition:enter="transition ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-x-8"
-        x-transition:enter-end="opacity-100 translate-x-0"
-        x-transition:leave="transition ease-in duration-150"
-        x-transition:leave-start="opacity-100 translate-x-0"
-        x-transition:leave-end="opacity-0 translate-x-8"
-        @keydown.escape.window="isOpen = false"
-
-        class="
-            flex justify-between max-w-xs sm:max-w-sm w-full
-            fixed bottom-0 right-0 bg-white rounded-xl shadow-lg border
-            px-4 py-5 mx-6 my-8 z-20
-        "
+        class="flex items-center"
     >
-        <div
-            class="flex items-center"
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
-            </svg>
+        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+        </svg>
 
 
-            <div class="font-semibold text-gray-500 text-sm sm:text-base ml-2">Ideea was updated successfully</div>
-        </div>
-        <button @click="isOpen = false" class="text-gray-400 hover:text-gray-500">
-            <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-
+        <div class="font-semibold text-gray-500 text-sm sm:text-base ml-2">Ideea was updated successfully</div>
     </div>
+    <button @click="isOpen = false" class="text-gray-400 hover:text-gray-500">
+        <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    </button>
 </div>
+
