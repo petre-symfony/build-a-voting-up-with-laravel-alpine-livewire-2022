f742487f68363c907fe1a6a83455cedd39ac164e
petrero <petrero@gmail.com>
2022-11-19 18:16:46 +0200

    48.5 php artisan livewire:make IdeaComment - Generate a livewire component to display a particular comment for an idea

---
+++ app/Http/Livewire/IdeaComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class IdeaComment extends Component {
+    public function render() {
+        return view('livewire.idea-comment');
+    }
+}
