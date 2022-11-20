8b70ee4646479a3913c5fed28699da85b110b5dc
petrero <petrero@gmail.com>
2022-11-20 11:18:41 +0200

    51.12 Remove n+1 issue problem when loading the comments in show page

--- app/Http/Livewire/IdeaComments.php
+++ app/Http/Livewire/IdeaComments.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use App\Models\Idea;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -24,7 +25,8 @@ class IdeaComments extends Component {
 
     public function render() {
         return view('livewire.idea-comments', [
-            'comments' => $this->idea->comments()->paginate()->withQueryString()
+            //'comments' => $this->idea->comments()->load('user')->paginate()->withQueryString() - dont work
+            'comments' => Comment::with('user')->where('idea_id', $this->idea->id)->paginate()->withQueryString()
         ]);
     }
 }
