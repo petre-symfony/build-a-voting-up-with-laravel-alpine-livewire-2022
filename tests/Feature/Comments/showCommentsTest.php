2f1a93a73575fcbb1b71e2e5261bd4d6763d615a
petrero <petrero@gmail.com>
2022-11-20 12:25:06 +0200

    52.3 Test the presence of idea-comment and idea-comments livewire components when idea has comments

--- tests/Feature/Comments/showCommentsTest.php
+++ tests/Feature/Comments/showCommentsTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature\Comments;
 
+use App\Models\Comment;
+use App\Models\Idea;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class showCommentsTest extends TestCase {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example() {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function idea_comments_livewire_component_renders() {
+        $idea = Idea::factory()->create();
+
+        $commentOne = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('idea-comments');
+    }
+
+    /** @test */
+    public function idea_comment_livewire_component_renders() {
+        $idea = Idea::factory()->create();
+
+        $commentOne = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('idea-comment');
     }
 }
