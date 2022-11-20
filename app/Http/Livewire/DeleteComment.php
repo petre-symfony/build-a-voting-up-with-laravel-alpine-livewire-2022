ea2da846e26048bda5751d5da80a3be91a1a2e7a
petrero <petrero@gmail.com>
2022-11-20 17:39:11 +0200

    54.2 php artisan livewire:make DeleteComment

---
+++ app/Http/Livewire/DeleteComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DeleteComment extends Component {
+    public function render() {
+        return view('livewire.delete-comment');
+    }
+}
