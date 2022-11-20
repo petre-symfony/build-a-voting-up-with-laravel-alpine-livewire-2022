f162525d8af94dd8ea4d71f3f38564853690bca1
petrero <petrero@gmail.com>
2022-11-20 14:15:03 +0200

    53.1 Altering comments must only be available for logged in users. Wrap that modal in @auth directive

--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -22,40 +22,42 @@
                     @endif
                     <div>{{ $comment->created_at->diffForHumans() }}</div>
                 </div>
-                <div
-                    x-data = "{ isOpen: false}"
-                    class="flex items-center space-x-2"
-                >
-                    <div class="relative">
-                        <button
-                            @click="isOpen = !isOpen"
-                            class="
-                                relative bg-gray-100 hover:bg-gray-200 border transition
-                                duration-150 ease-in rounded-full h-7 py-2 px-4
-                            "
-                        >
-                            <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                            </svg>
+                @auth
+                    <div
+                        x-data = "{ isOpen: false}"
+                        class="flex items-center space-x-2"
+                    >
+                        <div class="relative">
+                            <button
+                                @click="isOpen = !isOpen"
+                                class="
+                                    relative bg-gray-100 hover:bg-gray-200 border transition
+                                    duration-150 ease-in rounded-full h-7 py-2 px-4
+                                "
+                            >
+                                <svg class="text-gray-400 h-full scale-150" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
+                                    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                                </svg>
 
-                        </button>
-                        <ul
-                            x-cloak
-                            x-show="isOpen"
-                            x-transition.origin.top.left.duration.150ms
-                            @click.away="isOpen = false"
-                            @keydown.escape.window="isOpen = false"
-                            class="
-                                absolute w-44 text-left ml-8 font-semibold
-                                bg-white shadow-dialog rounded-xl py-3
-                                md:ml-8 top-8 md:top-6 right-0 md:left-0 z-10
-                            "
-                        >
-                            <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
-                            <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
-                        </ul>
+                            </button>
+                            <ul
+                                x-cloak
+                                x-show="isOpen"
+                                x-transition.origin.top.left.duration.150ms
+                                @click.away="isOpen = false"
+                                @keydown.escape.window="isOpen = false"
+                                class="
+                                    absolute w-44 text-left ml-8 font-semibold
+                                    bg-white shadow-dialog rounded-xl py-3
+                                    md:ml-8 top-8 md:top-6 right-0 md:left-0 z-10
+                                "
+                            >
+                                <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Mark as spam</a></li>
+                                <li><a href="" class="hover:bg-gray-100 block px-5 py-3">Delete Post</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                @endauth
             </div>
         </div>
     </div>
