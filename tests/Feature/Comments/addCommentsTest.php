77dd7b51e547f5c06c10be0229f2035944bbf5be
petrero <petrero@gmail.com>
2022-11-20 13:30:31 +0200

    52.9 Add comment form only renders when user is logged in test

--- tests/Feature/Comments/addCommentsTest.php
+++ tests/Feature/Comments/addCommentsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Comments;
 
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,5 +17,22 @@ class addCommentsTest extends TestCase {
 
         $response = $this->get(route('idea.show', $idea));
         $response->assertSeeLivewire('add-comment');
+    }
+
+    /** @test */
+    public function add_comment_form_renders_when_user_is_logged_in() {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('idea.show', $idea));
+        $response->assertSee("share your thoughts...");
+    }
+
+    /** @test */
+    public function add_comment_form_does_not_render_when_user_is_logged_out() {
+        $idea = Idea::factory()->create();
+
+        $response = $this->get(route('idea.show', $idea));
+        $response->assertSee('Please login or create an account to post a comment');
     }
 }
