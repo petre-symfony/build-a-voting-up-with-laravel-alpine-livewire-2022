4a66bf032fbd1d84777b5dc2268404ab0c2fddc3
petrero <petrero@gmail.com>
2022-11-20 14:21:14 +0200

    53.3 php artisan livewire:make EditComment

---
+++ app/Http/Livewire/EditComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EditComment extends Component {
+    public function render() {
+        return view('livewire.edit-comment');
+    }
+}
