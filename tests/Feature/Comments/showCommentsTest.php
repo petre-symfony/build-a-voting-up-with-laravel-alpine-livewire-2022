fdeddccdd0c5d90ca8c1a378d8af1cbb85be1277
petrero <petrero@gmail.com>
2022-11-20 12:57:17 +0200

    52.5 Test that the list of comments is seen on show page

--- tests/Feature/Comments/showCommentsTest.php
+++ tests/Feature/Comments/showCommentsTest.php
@@ -10,6 +10,7 @@ use Tests\TestCase;
 
 class showCommentsTest extends TestCase {
     use RefreshDatabase;
+    use WithFaker;
 
     /** @test */
     public function idea_comments_livewire_component_renders() {
@@ -43,5 +44,24 @@ class showCommentsTest extends TestCase {
 
         $this->get(route('idea.show', $idea))
             ->assertSee('No comments yet...');
+    }
+
+    /** @test */
+    public function list_of_comments_shows_on_idea_page() {
+        $idea = Idea::factory()->create();
+
+        $commentOne = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => $this->faker->words(5, true)
+        ]);
+
+        $commentTwo = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => $this->faker->words(5, true)
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeInOrder([$commentOne->body, $commentTwo->body])
+            ->assertSee('2 Comments');
     }
 }
