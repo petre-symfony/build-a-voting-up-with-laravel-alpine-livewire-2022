34e2223f22d5a5770218d9c934a54f7e53f073fa
petrero <petrero@gmail.com>
2022-11-21 17:09:19 +0200

    55.2 Comment is set correctly in edit comment livewire component

--- tests/Feature/Comments/EditCommentTest.php
+++ tests/Feature/Comments/EditCommentTest.php
@@ -3,18 +3,22 @@
 namespace Tests\Feature\Comments;
 
 use App\Http\Livewire\CreateIdea;
+use App\Http\Livewire\EditComment;
 use App\Http\Livewire\EditIdea;
 use App\Http\Livewire\IdeaShow;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Idea;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class EditCommentTest extends TestCase {
     use RefreshDatabase;
+    use WithFaker;
 
     /** @test */
     public function show_edit_comment_livewire_component_when_user_has_authorization() {
@@ -32,6 +36,24 @@ class EditCommentTest extends TestCase {
 
         $this->get(route('idea.show', $idea))
             ->assertDontSeeLivewire('edit-comment');
+    }
+
+    /** @test */
+    public function edit_comment_is_set_correctly_when_user_clicks_it_from_menu(){
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+            'body' => $this->faker->words(7, true)
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(EditComment::class)
+            ->call('setEditComment', $comment->id)
+            ->assertSet('body', $comment->body)
+            ->assertEmitted('editCommentWasSet');
     }
 
     /** @test */
