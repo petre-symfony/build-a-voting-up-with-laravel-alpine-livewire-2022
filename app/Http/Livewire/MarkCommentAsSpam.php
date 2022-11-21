ce8c6e9513dd67fb6022d6c25df5c89dc7dc50ae
petrero <petrero@gmail.com>
2022-11-21 19:04:46 +0200

    56.3 php artisan livewire:make MarkCommentAsSpam

---
+++ app/Http/Livewire/MarkCommentAsSpam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class MarkCommentAsSpam extends Component {
+    public function render() {
+        return view('livewire.mark-comment-as-spam');
+    }
+}
