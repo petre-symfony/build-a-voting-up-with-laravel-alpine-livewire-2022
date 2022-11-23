f8c0530845a97590986544ba9e73f76c69cef422
petrero <petrero@gmail.com>
2022-11-23 20:49:32 +0200

    59.1 Add a bell notification in our app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,15 +25,24 @@
                 @if (Route::has('login'))
                     <div class="px-6 py-4">
                         @auth
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+                            <div class="flex items-center space-x-4">
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
 
-                                <a href="{{route('logout') }}"
-                                   onclick="event.preventDefault(); this.closest('form').submit();"
-                                >
-                                    {{ __('Log Out') }}
-                                </a>
-                            </form>
+                                    <a href="{{route('logout') }}"
+                                       onclick="event.preventDefault(); this.closest('form').submit();"
+                                    >
+                                        {{ __('Log Out') }}
+                                    </a>
+                                </form>
+                                <div>
+                                    <button>
+                                        <svg viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                            <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
                         @else
                             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
