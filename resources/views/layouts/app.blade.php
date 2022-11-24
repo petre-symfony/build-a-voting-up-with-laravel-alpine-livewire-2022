3875db5cb42793863834674643031ee270c5996c
petrero <petrero@gmail.com>
2022-11-24 16:56:11 +0200

    59.6 Begin notification menu under the bell icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,8 +35,8 @@
                                         {{ __('Log Out') }}
                                     </a>
                                 </form>
-                                <div>
-                                    <button class="relative">
+                                <div class="relative">
+                                    <button>
                                         <svg viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-gray-400">
                                             <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd" />
                                         </svg>
@@ -48,6 +48,30 @@
                                             style="top: -6px; right: -6px"
                                         >8</div>
                                     </button>
+                                    <ul
+                                        {{--x-cloak --}}
+                                        x-show="isOpen"
+                                        x-transition.origin.top.left.duration.150ms
+                                        @click.away="isOpen=false"
+                                        @keydown.escape.window="isOpen = false"
+                                        class="
+                                            absolute w-44 text-left ml-8 font-semibold bg-white
+                                            shadow-dialog rounded-xl py-3
+                                            md:ml-8 top-8 md:top-6 right-0 md:left-0 z-10
+                                        "
+                                    >
+                                        <li>
+                                            <a
+                                                href="#"
+                                                class="hover:bg-gray-100 block px-5 py-3"
+                                                @click.prevent="
+                                                    isOpen = false
+                                                "
+                                            >
+                                                Edit Idea
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         @else
