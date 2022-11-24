a86b075cde94002628331b1515ad796dfe3d5770
petrero <petrero@gmail.com>
2022-11-24 22:52:45 +0200

    60.3 Deliver an email to the author of the idea when a comment for that idea is posted

--- app/Http/Livewire/AddComment.php
+++ app/Http/Livewire/AddComment.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Comment;
 use App\Models\Idea;
+use App\Notifications\CommentAdded;
 use Illuminate\Http\Response;
 use Livewire\Component;
 
@@ -26,7 +27,7 @@ class AddComment extends Component {
 
         $this->validate();
 
-        Comment::create([
+        $newComment = Comment::create([
            'user_id' => auth()->id(),
            'idea_id' => $this->idea->id,
            'status_id' => 1,
@@ -34,6 +35,8 @@ class AddComment extends Component {
         ]);
 
         $this->reset('comment');
+
+        $this->idea->user->notify(new CommentAdded($newComment));
 
         $this->emit('commentWasAdded', 'Comment was posted!');
     }
