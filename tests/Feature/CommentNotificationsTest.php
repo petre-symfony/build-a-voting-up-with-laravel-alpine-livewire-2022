132b09f8a5dd0bab07a9fe5bde50c0cc1aec5c34
petrero <petrero@gmail.com>
2022-11-27 00:10:45 +0200

    64.3 Test notifications are properly displayed. The second created comment is supposed to be displayed first, but this test will fail now because the comments are created in the same time

--- tests/Feature/CommentNotificationsTest.php
+++ tests/Feature/CommentNotificationsTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\AddComment;
+use App\Http\Livewire\CommentNotifications;
 use App\Models\Idea;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class CommentNotificationsTest extends TestCase {
@@ -28,5 +31,37 @@ class CommentNotificationsTest extends TestCase {
         $response = $this->get(route('idea.index'));
 
         $response->assertDontSeeLivewire('comment-notifications');
+    }
+
+    /** @test */
+    public function notifications_show_for_logged_in_user(){
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $userACommenting = User::factory()->create();
+        $userBCommenting = User::factory()->create();
+
+        Livewire::actingAs($userACommenting)
+            ->test(AddComment::class, [
+                'idea' => $idea
+            ])
+            ->set('comment', $this->faker->words(5, true))
+            ->call('addComment');
+
+
+        Livewire::actingAs($userBCommenting)
+            ->test(AddComment::class, [
+                'idea' => $idea
+            ])
+            ->set('comment', $this->faker->words(5, true))
+            ->call('addComment');
+
+        Livewire::actingAs($user)
+            ->test(CommentNotifications::class)
+            ->call('getNotifications')
+            ->assertSeeInOrder([$userBCommenting->comments()->first()->body, $userACommenting->comments()->first()->body])
+        ;
     }
 }
