9d51ac1c71ecdb4b2a67424425d90d89d604831b
petrero <petrero@gmail.com>
2022-11-26 23:00:00 +0200

    60.9 Notifications are only loaded when user clicks the bell icon. Even there'are no notifications yet a collection must be pass to comments notification livewire component at initialization, hence the existence of line $this->notifications = collect([])

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -5,9 +5,19 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 class CommentNotifications extends Component {
+    public $notifications;
+
+    protected $listeners = ['getNotifications'];
+
+    public function mount(){
+        $this->notifications = collect([]);
+    }
+
+    public function getNotifications(){
+        $this->notifications = auth()->user()->unreadNotifications;
+    }
+
     public function render() {
-        return view('livewire.comment-notifications', [
-            'notifications' => auth()->user()->unreadNotifications
-        ]);
+        return view('livewire.comment-notifications');
     }
 }
