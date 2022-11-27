e7440785db591ce6a754020aada5937f0a26de41
petrero <petrero@gmail.com>
2022-11-27 13:07:59 +0200

    66.3 Show avatar image when logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,10 +48,17 @@
                     </div>
                 @endif
                 <a href="">
-                    <img
-                        src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp"
-                        alt="avatar" class="w-10 h-10 rounded-full"
-                    >
+                    @auth
+                        <img
+                            src="{{ auth()->user()->getAvatar() }}"
+                            alt="avatar" class="w-10 h-10 rounded-full"
+                        >
+                    @else
+                        <img
+                            src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp"
+                            alt="avatar" class="w-10 h-10 rounded-full"
+                        >
+                    @endauth
                 </a>
             </div>
         </header>
